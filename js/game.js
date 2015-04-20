f86c1dc44b98de3ef4cc8dd7a1a816629c0bac33
Kurt Schnelker & suprfrye <pair+sabron13.suprfrye@devbootcamp.com>
2015-04-20 19:37:22 -0400

    Add method to filter zeros and add neighbors by direction

--- js/game.js
+++ js/game.js
@@ -30,10 +30,91 @@ Game.prototype = {
   toArray: function(chars) {
     var boardArray = [];
     for( var i = 0; i < 16; i += 4) {
-      var subarray = chars.slice(0 + i, 4 + i);
+      var subarray = chars.slice(i, 4 + i);
       boardArray.push(subarray.split(''));
     }
     return boardArray;
+  },
+
+  filterZeros: function(matrix) {
+    filtered = []
+    matrix.forEach(function(row) {
+      filtered.push(row.filter(function(value) {
+        return value !== '0';
+      }));
+    });
+    return filtered
+  },
+
+  leftAddNumbers: function(subarrays) {
+    subarrays.forEach(function(arr) {
+      if (arr.length > 1){
+        for(var i = 0; i < arr.length; i++){
+          if (arr[i] === arr[i+1]){
+            arr[i] = (parseInt(arr[i]) + parseInt(arr[i+1])).toString();
+            arr.splice(i+1, 1);
+          }
+        }
+      }
+    })
+    return subarrays;
+  },
+
+  rightAddNumbers: function(subarrays) {
+    subarrays.forEach(function(arr) {
+      if (arr.length > 1){
+        for(var i = arr.length - 1; i > 0; i--) {
+          if (arr[i] === arr[i-1]){
+            arr[i] = (parseInt(arr[i]) + parseInt(arr[i-1])).toString();
+            console.log(arr[i]);
+            arr.splice(i-1, 1);
+          }
+        }
+      }
+    })
+    return subarrays;
+
+  },
+
+  move: function(dir) {
+    switch (dir) {
+      case 'left':
+        this.board = this.leftAddNumbers(this.filterZeros(this.board))
+        this.board.forEach(function(row) {
+          for( var i = 0; row.length < 4; i++ ) {
+            row.push('0');
+          }
+        })
+        break;
+      case 'right':
+        this.board = this.rightAddNumbers(this.filterZeros(this.board))
+        this.board.forEach(function(row) {
+          for( var i = 0; row.length < 4; i++ ) {
+            row.unshift('0');
+          }
+        })
+        break;
+      case 'up':
+        var transposedArray = _.zip.apply(null, this.board);
+        transposedArray = this.filterZeros(transposedArray)
+        transposedArray.forEach(function(row) {
+          for( var i = 0; row.length < 4; i++ ) {
+            row.push('0');
+          }
+        })
+        this.board = _.zip.apply(null, transposedArray);
+        break;
+      case 'down':
+        var transposedArray = _.zip.apply(null, this.board);
+        transposedArray = this.filterZeros(transposedArray)
+        transposedArray.forEach(function(row) {
+          for( var i = 0; row.length < 4; i++ ) {
+            row.unshift('0');
+          }
+        })
+        this.board = _.zip.apply(null, transposedArray);
+        break;
+    }
   }
 };
 
